96b3162324aff95108a412c16428c9ec91303aca
yapie <targaryen.akane@gmail.com>
2020-07-21 18:31:33 +0700

    workbox runtimeCaching

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,14 +77,14 @@ export default {
     runtimeCaching: [
       {
         // Should be a regex string. Compiles into new RegExp('https://my-cdn.com/.*')
-        urlPattern: 'https://cdn.materialdesignicons.com/.*'
+        urlPattern: 'https://cdn.materialdesignicons.com/*'
         // Defaults to `networkFirst` if omitted
         // handler: 'networkFirst',
         // Defaults to `GET` if omitted
         // method: 'GET'
       },
       {
-        urlPattern: 'https://platform.twitter.com/.*'
+        urlPattern: 'https://platform.twitter.com/*'
       }
     ]
   },
